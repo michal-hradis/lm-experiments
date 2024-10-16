db8c9a37a809aba579581c619628614e720fcc41
Michal Hradis <michal.hradis@gmail.com>
2024-10-16 23:36:36 +0200

    Add script to extract texts form BUT-LLC dataset.

---
+++ tools/prepare_BUT-LLC.py
@@ -0,0 +1,30 @@
+import json
+import argparse
+
+
+def parseargs():
+    parser = argparse.ArgumentParser('Parse the jsonl files and extract the text.')
+    parser.add_argument('-i', '--input', required=True, help='Input jsonl file.')
+    parser.add_argument('-o', '--output', required=True, help='Output text file.')
+    parser.add_argument('--min-length', type=int, default=2000, help='Minimum length of each line.')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parseargs()
+    with open(args.input, 'r', encoding='utf-8') as f:
+        with open(args.output, 'w', encoding='utf-8') as f:
+        for line in f:
+            data = json.loads(line)
+            title = data['title']
+            text = data['text']
+            full_text = f'TEXT: {text}
+            if title:
+                full_text = f'TITLE: {title} ' + full_text
+            if len(full_text) >= args.min_length:
+                f.write(full_text + '\n')
+
+
+if __name__ == '__main__':
+    main()
