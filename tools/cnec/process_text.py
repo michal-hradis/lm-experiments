a49aa9825e7f3917a30a2abacff892591e48cb56
Michal Hradis <michal.hradis@gmail.com>
2024-10-19 11:22:25 +0200

    Batched llama inference.

--- tools/cnec/process_text.py
+++ tools/cnec/process_text.py
@@ -14,6 +14,24 @@ def parseargs():
     return args
 
 
+def process_output(output: str):
+    # Get text after "entity:"
+    output = output.split('entity:')[1]
+    if '<|EOS|>' not in output:
+        logging.warning(f'No <|EOS|> in output: {output}')
+    output = output.split('<|EOS|>')[0]
+    output = ' '.join(output.split())
+
+    # add spaces before all interpunctions if there is no space before them
+    interpunctions = ['.', ',', '!', '?', ':', ';', ')', ']', '}', '“', '„', '”']
+    for interpunction in interpunctions:
+        output = output.replace(interpunction, f' {interpunction}')
+
+    # remove multiple spaces
+    output = ' '.join(output.split())
+
+    return output
+
 def main():
     args = parseargs()
     logging.basicConfig(level=logging.INFO)
@@ -29,25 +47,20 @@ def main():
     model.config.torch_dtype = torch.bfloat16 # Otherwise, it crashes due to the value being a string "bfloat16"
 
     f_out = open(args.output, 'w', encoding='utf-8')
-    f_in = open(args.input, 'r', encoding='utf-8')
+    with open(args.input, 'r', encoding='utf-8') as f_in:
+        input_lines = [line.strip() for line in f_in.readlines()]
 
-    for i, line in tqdm(enumerate(f_in)):
-        prefix = prepare_prompt(line)
-        inputs = tokenizer(
-        [
-            prefix
-        ], return_tensors = "pt").to("cuda")
+    batch_size = 8
+    for i in tqdm(range(0, len(input_lines), batch_size)):
+        batch_lines = input_lines[i:i+batch_size]
+        prefixes = [prepare_prompt(line) for line in batch_lines]
+        inputs = tokenizer(prefixes, return_tensors = "pt").to("cuda")
         input_length = inputs['input_ids'].shape[1]
-
-        outputs = model.generate(**inputs, max_new_tokens=input_length * 1.3, use_cache=True)
-        output = tokenizer.batch_decode(outputs)[0]
-        # Get text after "entity:"
-        output = output.split('entity:')[1]
-        if '<|EOS|>' not in output:
-            logging.warning(f'No <|EOS|> in output: {output}')
-        output = output.split('<|EOS|>')[0]
-        output = ' '.join(output.split())
-        f_out.write(output + '\n')
+        outputs = model.generate(**inputs, max_new_tokens=input_length * 1.6 + 20, use_cache=True)
+        outputs = tokenizer.batch_decode(outputs)
+        for output in outputs:
+            output = process_output(output)
+            f_out.write(output + '\n')
 
 
 if __name__ == '__main__':
