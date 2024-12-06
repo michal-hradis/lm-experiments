719c6d0e5404f1dccde88ae962301260f6cc224d
Michal Hradis <michal.hradis@gmail.com>
2024-12-06 10:52:37 +0100

    Working text_for_annotation.py

--- dataset_tools/texts_for_annotation.py
+++ dataset_tools/texts_for_annotation.py
@@ -2,7 +2,8 @@ import argparse
 from glob import glob
 from random import sample, shuffle
 import os
-import md5
+import hashlib
+from tqdm import tqdm
 
 
 def parse_args():
@@ -14,7 +15,7 @@ def parse_args():
     parser.add_argument("--n", type=int, default=1, help="Number of lines to extract from each file.")
     parser.add_argument("--n-shared", type=int, default=1, help="Share every n-th file.")
     parser.add_argument("--n-directories", type=int, default=6, help="Number of directories to create.")
-    parser.add_arguemnt("--min-liine-length", type=int, default=13, help="Minimum words in a line to consider it.")
+    parser.add_argument("--min-line-length", type=int, default=13, help="Minimum words in a line to consider it.")
     return parser.parse_args()
 
 
@@ -24,11 +25,14 @@ def main():
     all_texts = []
 
     files = sorted(glob(os.path.join(args.input, '*.txt')))
-    for file in files:
+    for file in tqdm(files):
         with open(file, 'r', encoding='utf-8') as f:
             lines = [line.strip() for line in f.readlines() if len(line.split()) >= args.min_line_length]
-        for line in sample(lines, len(args.n)):
+        if len(lines) <= args.n:
+            continue
+        for line in sample(lines, args.n):
             all_texts.append((file, line))
+
 
 
     shared_files = all_texts[::(args.n_shared * args.n_directories)]
@@ -36,26 +40,32 @@ def main():
 
     # remove shared files from all_texts
     all_texts = [text for text in all_texts if text not in shared_files_set]
+    print(len(all_texts), len(shared_files))
 
-    groups = [all_texts[i:i + args.n] for i in range(args.n_directories)]
+    groups = [all_texts[i::args.n_directories] for i in range(args.n_directories)]
 
     # shuffle all groups and shared_files
     shuffle(shared_files)
     for group in groups:
         shuffle(group)
+        print(len(group))
+
 
     # interleave shared files equidistantly in each group.
     # Every n-the file in a group should be from shared_files
-    for group in enumerate(groups):
+    for group in groups:
+        print(len(group))
         for i in range(0, len(group), args.n_shared):
             group.insert(i, shared_files[i // args.n_shared])
+        print(len(group))
 
     for i, group in enumerate(groups):
         directory = os.path.join(args.output, f'{i}')
         os.makedirs(directory, exist_ok=True)
-        for j, (file, line) in enumerate(group):
-            file_hash = md5(file.encode()).hexdigest()
-            content_hash = md5(line.encode()).hexdigest()
+        print(len(group))
+        for j, (file_name, line) in enumerate(group):
+            file_hash = hashlib.md5(file_name.encode()).hexdigest()
+            content_hash = hashlib.md5(line.encode()).hexdigest()
             output_file_name = f'{j:05d}_{file_hash}_{content_hash}.txt'
             with open(os.path.join(directory, output_file_name), 'w', encoding='utf-8') as f_out:
                 f_out.write(line)
